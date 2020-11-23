862324e758f0aadec8aa72d6b5ef077eb5b10d6b
PranaviPendyala <pranavi.pendiyala@gmail.com>
2020-11-23 12:16:44 +0530

    Add-Recent event text

--- lib/screens/recent_events.dart
+++ lib/screens/recent_events.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:hues/utilities/constants.dart';
+
+
+
 class RecentEvents extends StatefulWidget {
   @override
   _RecentEventsState createState() => _RecentEventsState();
@@ -7,8 +12,40 @@ class RecentEvents extends StatefulWidget {
 class _RecentEventsState extends State<RecentEvents> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Container(
-      child: Text('Recent Events'),
+      child :  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal:size.width*0.01,vertical: size.height/(1200/80),),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 26,
+                  child: VerticalDivider(
+
+                    thickness: 3,
+                    color: yellowColor,
+                  ),
+                ),
+               Padding(
+                 padding: EdgeInsets.symmetric(horizontal: size.width*0.001),
+                 child: Text('Recent Events',
+                   style: TextStyle(fontWeight: FontWeight.bold,
+                     fontSize: 16,
+                   ),
+                 ),
+               ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
